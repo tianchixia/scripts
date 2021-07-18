627d3cd736c47e74058dd5111ba82b9e9c796925
tianchixia <71068929+tianchixia@users.noreply.github.com>
2021-07-18 23:38:42 +0800

    财富岛提现

---
+++ jdJxToken.ts
@@ -0,0 +1,24 @@
+/**
+ * 可以从cfd提现任意金额抓包
+ * url: https://m.jingxi.com/jxbfd/user/CashOut
+ * 从url中找参数的值
+ */
+let Jxtoken = [
+  {
+    strPgUUNum: "",
+    strPgtimestamp: "",
+    strPhoneID: ""
+  },
+  {
+    strPgUUNum: "",
+    strPgtimestamp: "",
+    strPhoneID: ""
+  },
+  {
+    strPgUUNum: "1e41d0ea8c1a9306ce8bd26241287b81",
+    strPgtimestamp: "1626620301183",
+    strPhoneID: "78bd780abd69bd1e18499c0cee8069c97b52af74"
+  },
+]
+
+export default Jxtoken
