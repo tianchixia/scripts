0960a04cf04e2bea27aeefaa914e762837dc5cf6
tianchixia <71068929+tianchixia@users.noreply.github.com>
2021-08-29 14:28:12 +0800

    京喜领88红包增加延时

--- jd_jxlhb.js
+++ jd_jxlhb.js
@@ -49,7 +49,7 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
   res = await getAuthorShareCode('https://raw.githubusercontent.com/Aaron-lv/updateTeam/master/shareCodes/jxhb.json')
   if (!res) {
     $.http.get({url: 'https://purge.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/jxhb.json'}).then((resp) => {}).catch((e) => $.log('刷新CDN异常', e));
-    await $.wait(2700)
+    await $.wait(3500)
     res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/jxhb.json')
   }
   if (res && res.activeId) $.activeId = res.activeId;
@@ -86,7 +86,7 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
       if ($.UserName === code['userName']) continue;
       console.log(`【${$.UserName}】去助力【${code['userName']}】邀请码：${code['strUserPin']}`);
       await enrollFriend(code['strUserPin']);
-      await $.wait(5000);
+      await $.wait(6000);
       if ($.max) continue
       if (!$.canHelp) break
     }
@@ -96,7 +96,7 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
         if (!item) continue;
         console.log(`【${$.UserName}】去助力作者的邀请码：${item}`);
         await enrollFriend(item);
-        await $.wait(5000);
+        await $.wait(6000);
         if ($.max) continue
         if (!$.canHelp) break
       }
@@ -111,7 +111,7 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
       if (!$.packetIdArr[i]) continue;
       console.log(`\n【${$.UserName}】去拆第${grade}个红包`);
       await openRedPack($.packetIdArr[i]['strUserPin'], grade);
-      await $.wait(4000);
+      await $.wait(6000);
     }
   }
 })()
@@ -123,9 +123,9 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
     })
 async function main() {
   await joinActive();
-  await $.wait(4000)
+  await $.wait(6000)
   await getUserInfo()
-  await $.wait(4000)
+  await $.wait(6000)
 }
 //参与活动
 function joinActive() {
