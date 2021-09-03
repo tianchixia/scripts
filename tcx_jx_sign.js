d035c4ccdff3dde7d6d6129031ca061c9337e8a7
tianchixia <71068929+tianchixia@users.noreply.github.com>
2021-09-03 17:27:44 +0800

    批量更新

--- tcx_jx_sign.js
+++ tcx_jx_sign.js
@@ -66,7 +66,7 @@ if ($.isNode()) {
         continue
       }
       if (i === 0) console.log(`\n正在收集助力码请等待\n`)
-      UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString()};network/wifi;model/iPhone10,2;appBuild/100609;ADID/00000000-0000-0000-0000-000000000000;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
+      UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100609;ADID/00000000-0000-0000-0000-000000000000;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
       await signhb(1)
       await $.wait(3500)
       UAInfo[$.UserName] = UA
@@ -96,7 +96,7 @@ if ($.isNode()) {
       }
       UA = UAInfo[$.UserName]
       await signhb(2)
-      await $.wait(3500)
+      await $.wait(4000)
       if ($.canHelp) {
         if ($.shareCodes && $.shareCodes.length) {
           console.log(`\n开始内部互助\n`)
@@ -116,7 +116,7 @@ if ($.isNode()) {
               continue
             }
             await helpSignhb($.shareCodes[j].smp)
-            await $.wait(3500)
+            await $.wait(4000)
             if (!$.black) $.shareCodes[j].num++
             break
           }
@@ -130,7 +130,7 @@ if ($.isNode()) {
           console.log("开始做红包任务")
           for (let j = 0; j < $.commonlist.length; j++) {
             await dotask($.commonlist[j]);
-            await $.wait(3000);
+            await $.wait(4000);
           }
         } else {
           console.log("红包任务已完成")
@@ -138,7 +138,7 @@ if ($.isNode()) {
         if ($.bxNum && $.bxNum.length) {
           for (let j = 0; j < $.bxNum[0].bxNum; j++) {
             await bxdraw()
-            await $.wait(3000)
+            await $.wait(4000)
           }
         }
         await doubleSign()
@@ -191,7 +191,7 @@ function signhb(type = 1) {
               for (let key of Object.keys(signlist)) {
                 let vo = signlist[key]
                 if (vo.istoday === 1) {
-                  if (vo.status === 1 && data.signtask.status === 1) {
+                  if (vo.status === 1 && data.todaysign === 1) {
                     console.log(`今日已签到`)
                     $.canHelp = false
                   } else {
@@ -240,7 +240,7 @@ function helpSignhb(smp = '') {
           for (let key of Object.keys(signlist)) {
             let vo = signlist[key]
             if (vo.istoday === 1) {
-              if (vo.status === 1 && data.signtask.status === 1) {
+              if (vo.status === 1 && data.todaysign === 1) {
                 // console.log(`今日已签到`)
               } else {
                 console.log(`此账号已黑`)
@@ -362,13 +362,14 @@ function taskUrl(functionId, body = '', stk) {
     }
   }
 }
-function randomString() {
-  return Math.random().toString(16).slice(2, 10) +
-    Math.random().toString(16).slice(2, 10) +
-    Math.random().toString(16).slice(2, 10) +
-    Math.random().toString(16).slice(2, 10) +
-    Math.random().toString(16).slice(2, 10)
+function randomString(e) {
+  e = e || 32;
+  let t = "0123456789abcdef", a = t.length, n = "";
+  for (let i = 0; i < e; i++)
+    n += t.charAt(Math.floor(Math.random() * a));
+  return n
 }
+
 
 function TotalBean() {
   return new Promise(async resolve => {
