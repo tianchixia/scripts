3c3eef4a0ac333ca168ad7e1bea87681b6d30236
tianchixia <71068929+tianchixia@users.noreply.github.com>
2021-09-22 16:30:41 +0800

    签到领现金兑换

--- tcx_cash_exchange.js
+++ tcx_cash_exchange.js
@@ -6,7 +6,6 @@
 const $ = new Env("签到领现金兑换B")
 const ua = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randomString(40)}`
 let cookiesArr = []
-let exchangeAccounts //不指定默认为所有账号兑换10红包，部分账号会出现参数错误的提示
 let exchangeAccounts = {
      "zhanxiachi": 10,//两元
      "wdNMhNkhpbxadIq": 10,//两元
