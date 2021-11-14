93bf2335251944d9681f5f7016056562dcac0b38
tianchixia <71068929+tianchixia@users.noreply.github.com>
2021-11-14 15:18:47 +0800

    JD入会领豆

--- jd_OpenCard.py
+++ jd_OpenCard.py
@@ -184,7 +184,7 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 pwd = os.path.dirname(os.path.abspath(__file__)) + os.sep
 
 ######
-openCardBean = 10
+openCardBean = 20
 sleepNum = 0.0
 record = True
 onlyRecord = False
